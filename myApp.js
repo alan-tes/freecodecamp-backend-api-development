db2920ae250dcd5e9a198aa5f25b587193bd6abf
Alan <alan.aquino@HzTelecom.local>
2025-06-30 15:27:55 -0300

    change

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
-let app = express();
+let bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+let app = express();
 
 
 
